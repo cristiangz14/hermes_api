d4e8c8a636f9fa9f7fc1be27cfe205d42f9df308
cristian <cristiangranada14@gmail.com>
2017-09-06 20:07:25 -0500

    extract validateUser as module

--- server/index.js
+++ server/index.js
@@ -3,19 +3,7 @@ const plugins = require('./config/plugins');
 const Hapi = require('hapi');
 const jwt = require('hapi-auth-jwt2');
 const jwksRsa = require('jwks-rsa');
-
-const validateUser = (decoded, request, callback) => {
-  // This is a simple check that the `sub` claim
-  // exists in the access token. Modify it to suit
-  // the needs of your application
-  if (decoded && decoded.sub) {
-    return callback(null, true, {
-      scope: decoded.scope.split(' ')
-    });
-  }
-
-  return callback(null, false);
-}
+const validateUser = require('./config/validateUser');
 
 const server = new Hapi.Server();
 
